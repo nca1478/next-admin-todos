7107bd03ca39ac9e4e2e5c9dd6a90fb5adda30d7
Nelson Cadenas <nca1478@yahoo.com>
2024-11-14 05:16:15 -0400

    113. Paginación simple

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -1,8 +1,28 @@
 import prisma from "@/lib/prisma";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET() {
-  const todos = await prisma.todos.findMany();
+export async function GET(request: NextRequest) {
+  const { searchParams } = new URL(request.url);
+  const take = Number(searchParams.get("take") ?? "10");
+  const skip = Number(searchParams.get("skip") ?? "0");
+
+  if (isNaN(take) || isNaN(skip)) {
+    return NextResponse.json(
+      {
+        success: false,
+        message: "Take o Skip tienen que ser números",
+      },
+      { status: 400 }
+    );
+  }
+
+  const todos = await prisma.todos.findMany({
+    skip,
+    take,
+    orderBy: {
+      createdAt: "asc",
+    },
+  });
 
   return NextResponse.json({
     success: true,
