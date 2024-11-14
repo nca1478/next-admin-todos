d52d1662929af74a376832dab6fdbed79335067e
Nelson Cadenas <nca1478@yahoo.com>
2024-11-14 16:07:09 -0400

    118. Validaciones en la actualización

--- src/app/api/todos/[id]/route.ts
+++ src/app/api/todos/[id]/route.ts
@@ -1,23 +1,31 @@
 import * as yup from "yup";
 import prisma from "@/lib/prisma";
 import { NextResponse, NextRequest } from "next/server";
+import { todos } from "@prisma/client";
 
 interface Segments {
   params: Promise<{ id: string }>;
 }
 
+interface ErrorResponse {
+  success: false;
+  error: string;
+}
+
+const getTodo = async (id: string): Promise<todos | null> => {
+  return await prisma.todos.findFirst({ where: { id } });
+};
+
 export async function GET(request: NextRequest, { params }: Segments) {
   const { id } = await params;
-  const todo = await prisma.todos.findFirst({ where: { id } });
 
+  const todo = await getTodo(id);
   if (!todo) {
-    return NextResponse.json(
-      {
-        success: false,
-        message: `Todo con id ${id} no encontrado`,
-      },
-      { status: 404 }
-    );
+    const response: ErrorResponse = {
+      success: false,
+      error: `Todo con id ${id} no encontrado`,
+    };
+    return NextResponse.json(response, { status: 404 });
   }
 
   return NextResponse.json({
@@ -26,29 +34,38 @@ export async function GET(request: NextRequest, { params }: Segments) {
   });
 }
 
+const postSchema = yup.object({
+  description: yup.string().optional(),
+  complete: yup.boolean().optional().default(false),
+});
+
 export async function PUT(request: NextRequest, { params }: Segments) {
   const { id } = await params;
 
-  const todo = await prisma.todos.findFirst({ where: { id } });
+  const todo = await getTodo(id);
   if (!todo) {
-    return NextResponse.json(
-      {
-        success: false,
-        message: `Todo con id ${id} no encontrado`,
-      },
-      { status: 404 }
-    );
+    const response: ErrorResponse = {
+      success: false,
+      error: `Todo con id ${id} no encontrado`,
+    };
+    return NextResponse.json(response, { status: 404 });
   }
 
-  const body = await request.json();
+  try {
+    const { complete, description } = await postSchema.validate(
+      await request.json()
+    );
 
-  const todoUpdated = await prisma.todos.update({
-    where: { id },
-    data: body,
-  });
+    const todoUpdated = await prisma.todos.update({
+      where: { id },
+      data: { complete, description },
+    });
 
-  return NextResponse.json({
-    success: true,
-    data: todoUpdated,
-  });
+    return NextResponse.json({
+      success: true,
+      data: todoUpdated,
+    });
+  } catch (error) {
+    return NextResponse.json({ success: false, error }, { status: 400 });
+  }
 }
