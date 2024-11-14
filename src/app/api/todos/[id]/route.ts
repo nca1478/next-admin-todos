c4aef99b489fd762e1f63fdc9c7106d6eb5d0a0e
Nelson Cadenas <nca1478@yahoo.com>
2024-11-14 15:29:22 -0400

    117. Actualizar entradas - PUT

--- src/app/api/todos/[id]/route.ts
+++ src/app/api/todos/[id]/route.ts
@@ -1,3 +1,4 @@
+import * as yup from "yup";
 import prisma from "@/lib/prisma";
 import { NextResponse, NextRequest } from "next/server";
 
@@ -22,5 +23,32 @@ export async function GET(request: NextRequest, { params }: Segments) {
   return NextResponse.json({
     success: true,
     data: todo,
+  });
+}
+
+export async function PUT(request: NextRequest, { params }: Segments) {
+  const { id } = await params;
+
+  const todo = await prisma.todos.findFirst({ where: { id } });
+  if (!todo) {
+    return NextResponse.json(
+      {
+        success: false,
+        message: `Todo con id ${id} no encontrado`,
+      },
+      { status: 404 }
+    );
+  }
+
+  const body = await request.json();
+
+  const todoUpdated = await prisma.todos.update({
+    where: { id },
+    data: body,
+  });
+
+  return NextResponse.json({
+    success: true,
+    data: todoUpdated,
   });
 }
