fa68223969004d23031790a5e214a8a1eb14ceaa
Nelson Cadenas <nca1478@yahoo.com>
2024-11-15 15:48:38 -0400

    131. Actualizar Todo

--- src/todos/components/todosGrid/TodosGrid.tsx
+++ src/todos/components/todosGrid/TodosGrid.tsx
@@ -1,5 +1,8 @@
+"use client";
+
 import { Todo } from "@prisma/client";
 import TodoItem from "../todoItem/TodoItem";
+import * as api from "@/todos/helpers/todos";
 
 interface Props {
   todos?: Todo[];
@@ -9,7 +12,7 @@ export default function TodosGrid({ todos = [] }: Props) {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-3 gap-2">
       {todos.map((todo) => (
-        <TodoItem key={todo.id} todo={todo} />
+        <TodoItem key={todo.id} todo={todo} toggleTodo={api.updateTodo} />
       ))}
     </div>
   );
---
+++ src/todos/helpers/todos.ts
@@ -0,0 +1,18 @@
+import { Todo } from "@prisma/client";
+
+export const updateTodo = async (
+  id: string,
+  complete: boolean
+): Promise<Todo> => {
+  const body = { complete };
+
+  const todo = await fetch(`http://localhost:3000/api/todos/${id}`, {
+    method: "PUT",
+    body: JSON.stringify(body),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((res) => res.json());
+
+  return todo;
+};
