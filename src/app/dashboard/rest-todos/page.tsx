e2f2e51b93f4ff148930d72a74d384c147867895
Nelson Cadenas <nca1478@yahoo.com>
2024-11-15 04:16:35 -0400

    128. Listar las entradas

--- src/app/dashboard/rest-todos/page.tsx
+++ src/app/dashboard/rest-todos/page.tsx
@@ -1,7 +1,25 @@
-export default function RestTodosPage() {
+import prisma from "@/lib/prisma";
+
+export const metadata = {
+  title: "Listado de Todos",
+  description: "Listado de Todos",
+};
+
+export default async function RestTodosPage() {
+  const todos = await prisma.todos.findMany({
+    orderBy: { description: "asc" },
+  });
+
+  // useEffect(() => {
+  //   fetch("/api/todos")
+  //     .then((resp) => resp.json())
+  //     .then(console.log);
+  // }, []);
+
   return (
     <div>
       <h1 className="text-2xl">Rest Todos Page</h1>
+      {JSON.stringify(todos)}
     </div>
   );
 }
