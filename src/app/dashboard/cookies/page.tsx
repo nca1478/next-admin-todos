f2f8df7bca594bb826e5984a0c29f7f050093b88
Nelson Cadenas <nca1478@yahoo.com>
2024-11-19 04:46:17 -0400

    155. Cookies - Server Side

--- src/app/dashboard/cookies/page.tsx
+++ src/app/dashboard/cookies/page.tsx
@@ -1,16 +1,22 @@
 import { TabBar } from "@/components";
+import { cookies } from "next/headers";
 
 export const metadata = {
   title: "Cookies Page",
   description: "Cookies Page",
 };
 
-export default function CookiesPage() {
+export default async function CookiesPage() {
+  const cookieStore = await cookies();
+  const cookieTab = cookieStore.get("selectedTab")?.value ?? "1";
+  // const allCookies = cookieStore.getAll();
+
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 gap-3">
       <div className="flex flex-col">
         <span className="text-3xl">Tabs</span>
-        <TabBar />
+        <TabBar currentTab={+cookieTab} />
+        {/* {JSON.stringify(allCookies)} */}
       </div>
     </div>
   );
