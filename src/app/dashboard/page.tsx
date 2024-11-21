66f47c0370ec74b1cce618612cc0b9d3a68c2de7
Nelson Cadenas <nca1478@yahoo.com>
2024-11-21 03:36:08 -0400

    163. Total + impuesto

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,7 +3,11 @@ import { WidgetItem } from "@/components";
 export default function DashboardPage() {
   return (
     <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
-      <WidgetItem />
+      <WidgetItem title="Home">
+        <div className="flex flex-col items-center">
+          <span>Hola Mundo!!!</span>
+        </div>
+      </WidgetItem>
     </div>
   );
 }
