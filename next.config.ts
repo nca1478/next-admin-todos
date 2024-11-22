3987b527807efaaddff22b7ea307cb43356d0ecd
Nelson Cadenas <nca1478@yahoo.com>
2024-11-22 15:51:41 -0400

    175. Google Provider

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "avatars.githubusercontent.com",
       },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
     ],
   },
 };
